f83bc99f74a3bb3c3e74cea0bdb32ef57a2af4dc
PietroFiori91 <pederflowers@hotmail.it>
2023-07-28 15:43:06 +0200

    mail gen

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      mailList: [],
+    };
+  },
+
+  methods: {},
+
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((axiosResp) => {
+          this.mailList.push(axiosResp.data.response);
+        });
+    }
+  },
+}).mount("#app");
